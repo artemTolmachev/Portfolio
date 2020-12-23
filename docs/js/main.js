055f56ed5b7cf0de7d3fb7f405d8c8fb874640ef
Artem <lidama65@mail.ua>
2020-12-23 14:16:17 +0300

    Portfolio

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,24 @@
+
+
 const toggle = document.querySelector('.toggle-menu');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('active');
+
 });
 
+
+
+let containerEl = document.querySelector('#portfolio-projects');
+let mixer = mixitup(containerEl, {
+    classNames: {
+        block: ""
+    }
+});
+ 
+$(function() {
+    $("#btn .portfolio-nav_projects").click(function() {
+        $("#btn .portfolio-nav_projects").removeClass("active");         
+        $(this).toggleClass("active");
+    })
+});
